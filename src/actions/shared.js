e11e76a4825a8b36474cfa0ca2185ed78dd1dada
Dieter Jackson <jacksondieter@gmail.com>
2020-05-05 23:34:30 +0200

    feat: Adding reducer

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import { receiveUsers } from './users';
+import { receiveQuestions } from './questions';
+import { getInitialData } from '../utils/api';
+
+export function handleInitialData () {
+    return (dispatch) => {
+        // dispatch loading
+        return getInitialData()
+            .then(({ users, questions }) => {
+                // dispatch end of loading
+                dispatch(receiveUsers(users))
+                dispatch(receiveQuestions(questions))
+            })
+    }
+  }
